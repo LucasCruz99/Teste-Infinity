49152a9b9bfed8a1a36c600e5da9f7c8474c6d95
Lucas Cruz <55255048+LucasCruz99@users.noreply.github.com>
2022-06-28 12:06:10 -0300

    Dia 1

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody PlayerRigidbody;
+    public float Vel;
+    public GameObject Look;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+    private void FixedUpdate()
+    {
+        Movimento();
+    }
+    void Movimento()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit raycastHit;
+
+        PlayerRigidbody.transform.position += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * Vel;
+        PlayerRigidbody.transform.LookAt(Look.transform, Look.transform.up);
+
+        if (Physics.Raycast(ray, out raycastHit))
+            Look.transform.position = raycastHit.point;
+
+    }
+
+    void SetAnimation()
+    {
+
+
+
+
     }
 }
